a1868d2159c84fab2e42be6e87e5d20dfebff642
Khai Pham Quoc <pqkhai@tma.com.vn>
2025-10-02 17:04:22 +0700

    on going

--- MinTrackingStackImplementation.go
+++ MinTrackingStackImplementation.go
@@ -1,9 +1,56 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func processCouponStackOperations(operations []string) []int32 {
 	// Write your code here
-	func getOper(operation string) {
+	stack := []int32{}
+	result := []int32{}
+	var min int32 = 0
+	findMin := func(arr []int32) int32 {
+		var min int32 = 0
+		if len(arr) > 0 {
+			min = arr[0]
+		}
+		for _, val := range arr {
+			if val < min {
+				min = val
+			}
+		}
+		return min
+	}
+	for _, operation := range operations {
+		args := strings.SplitN(operation, " ", 2)
+		switch args[0] {
+		case "push":
+			num, _ := strconv.Atoi(args[1])
+			if len(stack) < 1 {
+				min = int32(num)
+			}
+			if int32(num) < min {
+				min = int32(num)
+			}
+			stack = append(stack, int32(num))
+		case "pop":
+			if len(stack) > 0 {
+				top := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				if top == min {
+					min = findMin(stack)
+				}
+			}
+		case "top":
+			if len(stack) > 0 {
+				top := stack[len(stack)-1]
+				result = append(result, top)
+			}
+		case "getMin":
+			result = append(result, min)
+		}
 
 	}
-	for operation := operations {}
+	return result
 }
